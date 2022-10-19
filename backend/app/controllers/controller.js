cac08ef012108d614946a0d79596d4b873d0d1f0
Gonzalo Seriche <gonzalo.seriche@gmail.com>
2022-10-19 00:24:00 -0300

    backend basico listo, queda definir el CRUD

---
+++ backend/app/controllers/controller.js
@@ -0,0 +1,22 @@
+const db = require("../models");
+const Tutorial = db.tutorials;
+
+exports.create = (req, res) => {
+
+};
+
+exports.findAll = (req, res) => {
+
+};
+
+exports.findOne = (req, res) => {
+
+};
+
+exports.update = (req, res) => {
+
+};
+
+exports.delete = (req, res) => {
+    
+}
